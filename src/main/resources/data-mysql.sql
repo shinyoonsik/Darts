629a4d3bf55343ea34f87c63dd985eb9fbacf8fd
yoonsik(for private project) <yoonsik@sin-yunsig-ui-MacBookPro.local>
2023-10-11 17:13:52 +0900

    데이터베이스 초기화 schema.sql, data.sql

---
+++ src/main/resources/data-mysql.sql
@@ -0,0 +1,4 @@
+INSERT INTO `security`.`score`(`score`,`student_id`,`subject`)VALUES(100,1,'english');
+INSERT INTO `security`.`score`(`score`,`student_id`,`subject`)VALUES(90,1,'math');
+
+INSERT INTO `security`.`student`(`age`,`name`)VALUES(33,'ys');
---
+++ src/main/resources/schema/schema-mysql.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `memberAA` (
+                          `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'test',
+                          `name` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+                          `password` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+                          PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
